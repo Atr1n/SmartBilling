93faadd953a6dc66f85d70cabcc426371ba8e9c8
maxim <xn_atr1n@mail.ru>
2022-04-08 19:00:30 +0600

    first commit

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,9 +1,9 @@
-const navToggle = document.querySelector('#navToggle')
-const overlay = document.querySelector('.overlay')
-const body = document.querySelector('body')
+// const navToggle = document.querySelector('#navToggle')
+// const overlay = document.querySelector('.overlay')
+// const body = document.querySelector('body')
 
-navToggle.addEventListener('click', () => {
-    navToggle.classList.toggle('active')
-    overlay.classList.toggle('open')
-    body.classList.toggle('locked')
-})
+// navToggle.addEventListener('click', () => {
+//     navToggle.classList.toggle('active')
+//     overlay.classList.toggle('open')
+//     body.classList.toggle('locked')
+// })
