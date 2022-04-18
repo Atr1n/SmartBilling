eb1d68daf1b3899db47077b544751d42198581ac
maxim <xn_atr1n@mail.ru>
2022-04-18 18:07:34 +0600

    mobile done

--- src/js/components/burger.js
+++ src/js/components/burger.js
@@ -1,9 +1,26 @@
-// const navToggle = document.querySelector('#navToggle')
-// const overlay = document.querySelector('.overlay')
-// const body = document.querySelector('body')
+const navToggle = document.querySelector('#navToggle')
+const overlay = document.querySelector('.overlay')
+const body = document.querySelector('body')
 
-// navToggle.addEventListener('click', () => {
-//     navToggle.classList.toggle('active')
-//     overlay.classList.toggle('open')
-//     body.classList.toggle('locked')
-// })
+navToggle.addEventListener('click', () => {
+    navToggle.classList.toggle('active')
+    overlay.classList.toggle('open')
+    body.classList.toggle('locked')
+})
+
+let scrollpos = window.scrollY
+const main = document.querySelector('main')
+const header_height = main.offsetHeight
+
+const add_class_on_scroll = () => navToggle.classList.add('mn')
+const remove_class_on_scroll = () => navToggle.classList.remove('mn')
+
+window.addEventListener('scroll', function() {
+    scrollpos = window.scrollY
+
+    if (scrollpos >= header_height) {
+        add_class_on_scroll()
+    } else {
+        remove_class_on_scroll()
+    }
+})
